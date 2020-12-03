985a0fd9b79730d7db7476bfdb78d5cca1cfb607
Danny Blaker <dannyb@semsci.com>
2020-12-03 12:16:47 +1100

    refactor

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import cleantext
-
+import pendulum
 if __name__ == '__main__':
-    c_text = cleantext.clean('This is A s$ample !!!!tExt3% to  cleaN566556+2+59*/133', extra_spaces=True,
-                             lowercase=True,
-                             numbers=True, punct=True)
-    print(c_text)
-    print("THIS IS STAGING")
+    now = pendulum.now("Europe/Paris")
+    # Changing timezone
+    print(now.in_timezone("America/Toronto"))
+    # Default support for common datetime formats
+    print(now.to_iso8601_string())
+    # Shifting
+    print(now.add(days=2))
+    print(now)
